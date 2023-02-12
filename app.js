8ca9b1091eefc589b7a94afc4071e0867ea577ec
Wasiu Bakare <wasiubakare@gmail.com>
2023-02-12 12:12:28 +0100

    Done with the geocoding and forecasting feature

---
+++ app.js
@@ -0,0 +1,21 @@
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const address = process.argv[2];
+
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  geocode(address, (err, { latitude, longitude, location }) => {
+    if (err) {
+      return console.log(err);
+    }
+    forecast(latitude, longitude, (err, forecastData) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
